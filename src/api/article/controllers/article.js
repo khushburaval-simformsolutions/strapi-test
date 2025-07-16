0f946c4fc6dce76d4b8d30663645637836bd94df
Kishan-Das_ivanti <kishan.das@ivanti.com>
2025-07-16 21:48:02 +0530

    add published count logic

--- src/api/article/controllers/article.js
+++ src/api/article/controllers/article.js
@@ -4,6 +4,26 @@
  * article controller
  */
 
+// const { createCoreController } = require('@strapi/strapi').factories;
+
+// module.exports = createCoreController('api::article.article');
+
+
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::article.article');
+module.exports = createCoreController('api::article.article', ({ strapi }) => ({
+  // Add your custom controller method here
+  async publishedCount(ctx) {
+    try {
+      // Call the custom service method
+      const count = await strapi.service('api::article.article').getPublishedCount();
+
+      // Return the count as a JSON response
+      ctx.body = { publishedCount: count };
+    } catch (err) {
+      // Handle any errors that occur
+      ctx.body = err;
+      ctx.status = 500; // Internal Server Error
+    }
+  },
+}));
--- src/api/article/routes/article.js
+++ src/api/article/routes/article.js
@@ -4,6 +4,63 @@
  * article router
  */
 
-const { createCoreRouter } = require('@strapi/strapi').factories;
+// const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::article.article');
+// module.exports = createCoreRouter('api::article.article');
+
+
+module.exports = {
+    routes: [
+      // --- Add this new custom route ---
+      {
+        method: 'GET',
+        path: '/articles/published-count', // Your custom endpoint
+        handler: 'api::article.article.publishedCount', // Points to the new controller method
+        config: {
+          policies: [], // You can add policies here to restrict access if needed
+          auth: false // Set to true if authentication is required
+        },
+      },
+      // ---------------------------------
+      {
+        method: 'GET',
+        path: '/articles',
+        handler: 'api::article.article.find',
+        config: {
+          policies: [],
+        },
+      },
+      {
+        method: 'GET',
+        path: '/articles/:id',
+        handler: 'api::article.article.findOne',
+        config: {
+          policies: [],
+        },
+      },
+      {
+        method: 'POST',
+        path: '/articles',
+        handler: 'api::article.article.create',
+        config: {
+          policies: [],
+        },
+      },
+      {
+        method: 'PUT',
+        path: '/articles/:id',
+        handler: 'api::article.article.update',
+        config: {
+          policies: [],
+        },
+      },
+      {
+        method: 'DELETE',
+        path: '/articles/:id',
+        handler: 'api::article.article.delete',
+        config: {
+          policies: [],
+        },
+      },
+    ],
+  };
